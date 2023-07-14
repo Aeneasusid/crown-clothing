5c1817fc5c9417f86dd2039dd38418b44bb41382
Aeneasusid <aeneasusid@gmail.com>
2023-07-15 00:37:05 +0800

    checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -11,18 +11,17 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
 
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => navigate('/checkout');
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
-        ) : (
-          <span className='empty-message'>Your cart is empty</span>
-        )}
+      {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button  onClick={goToCheckoutHandler}>TO CHECKOUT</Button>
     </div>
   );
 };
